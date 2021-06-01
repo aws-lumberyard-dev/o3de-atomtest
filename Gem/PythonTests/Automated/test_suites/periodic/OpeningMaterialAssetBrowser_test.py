6d31564f67d67b797f4587565440b8b732c0e5a4
darapan <darapan@amazon.com>
2021-06-01 05:17:01 -0700

    "Fixing review comments"

--- Gem/PythonTests/Automated/test_suites/periodic/OpeningMaterialAssetBrowser_test.py
+++ Gem/PythonTests/Automated/test_suites/periodic/OpeningMaterialAssetBrowser_test.py
@@ -6,6 +6,9 @@ distribution (the "License"). All use of this software is governed by the Licens
 or, if provided, by the license below or the license accompanying this file. Do not
 remove or modify any license notices. This file is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+
+
+Opening Material via Asset Browser
 """
 
 import os
@@ -14,52 +17,41 @@ import pytest
 from Automated.atom_utils import hydra_test_utils as hydra
 from Automated.atom_utils.automated_test_base import TestAutomationBase
 
-test_directory = os.path.dirname(__file__)
-log_monitor_timeout = 60
+TEST_DIRECTORY = os.path.dirname(__file__)
+LOG_MONITOR_TIMEOUT = 60
 
 
 @pytest.mark.parametrize("project", ["AtomTest"])
 @pytest.mark.parametrize("launcher_platform", ["windows_generic"])
 @pytest.mark.system
 class TestOpeningMaterialAssetBrowser(TestAutomationBase):
-    @pytest.mark.test_case_id("C34448135")
     @pytest.mark.parametrize("exe_file_name", ["MaterialEditor"])
     def test_OpeningMaterialAssetBrowser(
-        self, request, project, workspace,  launcher_platform, generic_launcher, exe_file_name, golden_images_directory
+        self, request, project, workspace, launcher_platform, generic_launcher, exe_file_name
     ):
-        golden_screenshot = [
-            os.path.join(golden_images_directory, "Windows", "basic_grey.ppm"),
-        ]
-
-        test_screenshot = [
-            os.path.join(
-                workspace.paths.engine_root(),
-                project,
-                "Cache",
-                "pc",
-                "screenshots",
-                "screenshot_basic_grey.ppm",
-            ),
-        ]
-
-        self.remove_artifacts(test_screenshot)
 
         expected_lines = [
             "Asset Browser opened: True",
             "basic_grey.material asset is filtered in Asset Browser",
-            "Screenshot taken.",
+            "basic_grey.material opened in viewport: True"
+        ]
+
+        unexpected_lines = [
+            "Trace::Assert",
+            "Trace::Error",
+            "Traceback (most recent call last):",
+            "basic_grey.material opened in viewport: False"
         ]
 
         hydra.launch_and_validate_results(
             request,
-            test_directory,
+            TEST_DIRECTORY,
             generic_launcher,
             "OpeningMaterialAssetBrowser_test_case.py",
-            timeout=log_monitor_timeout,
+            timeout=LOG_MONITOR_TIMEOUT,
             expected_lines=expected_lines,
+            unexpected_lines=unexpected_lines,
             halt_on_unexpected=True,
             log_file_name="MaterialEditor.log",
         )
 
-        for test_screenshot, golden_screenshot in zip(test_screenshot, golden_screenshot):
-            self.compare_screenshots(test_screenshot, golden_screenshot)
--- Gem/PythonTests/Automated/test_suites/periodic/OpeningMaterialAssetBrowser_test_case.py
+++ Gem/PythonTests/Automated/test_suites/periodic/OpeningMaterialAssetBrowser_test_case.py
@@ -9,7 +9,11 @@ remove or modify any license notices. This file is distributed on an "AS IS" BAS
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 
 
-Test Case Title : Opening Material via Asset Browser
+Opening Material via Asset Browser
+
+import azlmbr.materialeditor will fail with a ModuleNotFound error when using this script with Editor.exe
+This is because azlmbr.materialeditor only binds to MaterialEditor.exe and not Editor.exe
+You need to launch this script with MaterialEditor.exe in order for azlmbr.materialeditor to appear.
 """
 
 import os
@@ -23,9 +27,7 @@ import azlmbr.materialeditor.general as general
 sys.path.append(os.path.join(azlmbr.paths.devroot, "AtomTest", "Gem", "PythonTests"))
 
 import Automated.atom_utils.material_editor_utils as material_editor
-from Automated.atom_utils.material_editor_utils import MaterialEditorHelper, capture_screenshot
-
-screenshotsFolder = os.path.join(azlmbr.paths.devroot, "AtomTest", "Cache", "pc", "screenshots")
+from Automated.atom_utils.material_editor_utils import MaterialEditorHelper
 
 
 class TestOpeningMaterialAssetBrowser(MaterialEditorHelper):
@@ -37,13 +39,21 @@ class TestOpeningMaterialAssetBrowser(MaterialEditorHelper):
         Summary:
         Opening Material via Asset Browser
 
+        Test Steps:
+        1) Open Asset Browser
+        2) Search asset "basic_grey" in Material Browser
+        3) Make sure basic_grey.material asset is filtered in asset browser
+        4) Click on basic_grey.material in asset browser
+        5) Make sure basic_grey.material opened in the viewport without issue
+
+
         Expected Result:
         Basic_grey.material opens in the viewport without issue.
 
         :return: None
         """
 
-        # Open Asset Browser
+        # 1) Open Asset Browser
         pane_name = "Asset Browser"
         result = material_editor.is_pane_visible(pane_name)
         if not result:
@@ -52,29 +62,37 @@ class TestOpeningMaterialAssetBrowser(MaterialEditorHelper):
         print(f"{pane_name} opened: {result}")
         general.idle_wait_frames(3)
 
-        # Search asset "basic_grey" in Material Browser
+        # 2) Search asset "basic_grey" in Material Browser
         editor_window = pyside_utils.get_editor_main_window()
         asset_browser = editor_window.findChildren(QtWidgets.QWidget, "Asset Browser")[0]
         search_bar = asset_browser.findChildren(QtWidgets.QLineEdit, "textSearch")[0]
         search_bar.setText("basic_grey.material")
         general.idle_wait_frames(1)
         asset_browser_tree = asset_browser.findChild(QtWidgets.QTreeView, "m_assetBrowserTreeViewWidget")
-        model_index = pyside_utils.find_child_by_pattern(asset_browser_tree, "basic_grey.material")    
+        model_index = pyside_utils.find_child_by_pattern(asset_browser_tree, "basic_grey.material")
 
-        # Make sure basic_grey.material asset is filtered in asset browser
+        # 3) Make sure basic_grey.material asset is filtered in asset browser
         if (asset_browser_tree.indexBelow(model_index)) == (QtCore.QModelIndex()) and model_index is not None:
             print("basic_grey.material asset is filtered in Asset Browser")
-        
-        # Click on basic_grey.material in asset browser
+
+        # 4) Click on basic_grey.material in asset browser
         asset_browser_tree.expandAll()
         model_index = pyside_utils.find_child_by_pattern(asset_browser_tree, "basic_grey.material")
         asset_browser_tree.scrollTo(model_index)
         pyside_utils.item_view_index_mouse_click(asset_browser_tree, model_index)
-        QtTest.QTest.mouseDClick(asset_browser_tree.viewport(), QtCore.Qt.LeftButton, QtCore.Qt.NoModifier, asset_browser_tree.visualRect(model_index).center())
+        QtTest.QTest.mouseDClick(
+            asset_browser_tree.viewport(),
+            QtCore.Qt.LeftButton,
+            QtCore.Qt.NoModifier,
+            asset_browser_tree.visualRect(model_index).center(),
+        )
 
-        # Capture Screenshot
-        capture_screenshot(os.path.join(screenshotsFolder, "screenshot_basic_grey.ppm"))
-        
+        # 5) Make sure basic_grey.material opened in the viewport without issue
+        tab_widget = editor_window.findChild(QtWidgets.QTabWidget, "TabWidget")
+        tab_bar = tab_widget.findChild(QtWidgets.QTabBar)
+        self.wait_for_condition(lambda: tab_bar.count() == 1)
+        print(f"basic_grey.material opened in viewport: {tab_bar.count()==1}")
+
 
 test = TestOpeningMaterialAssetBrowser()
 test.run()
