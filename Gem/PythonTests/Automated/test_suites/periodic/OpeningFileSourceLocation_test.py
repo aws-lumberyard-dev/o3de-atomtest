7904cf40bf821791d859d1c3b97fd53ca1852934
darapan <darapan@amazon.com>
2021-06-14 23:43:17 -0700

    "Fixing review comments"

--- Gem/PythonTests/Automated/test_suites/periodic/OpeningFileSourceLocation_test.py
+++ Gem/PythonTests/Automated/test_suites/periodic/OpeningFileSourceLocation_test.py
@@ -6,9 +6,6 @@ distribution (the "License"). All use of this software is governed by the Licens
 or, if provided, by the license below or the license accompanying this file. Do not
 remove or modify any license notices. This file is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-
-
-Opening File Source Location
 """
 
 import os
@@ -20,31 +17,33 @@ from Automated.atom_utils.automated_test_base import TestAutomationBase
 TEST_DIRECTORY = os.path.dirname(__file__)
 LOG_MONITOR_TIMEOUT = 60
 
-
 @pytest.mark.parametrize("project", ["AtomTest"])
 @pytest.mark.parametrize("launcher_platform", ["windows_generic"])
-@pytest.mark.system
-class TestOpeningMaterialAssetBrowser(TestAutomationBase):
+class TestOpeningFileSourceLocation(TestAutomationBase):
     @pytest.fixture(autouse=True)
     def setup_teardown(self, request):
         def teardown():
+            # Close the beach_parking_1k_iblglobalcm.exr material opened in WindowsExplorer
             os.system('TASKKILL /F /FI "WINDOWTITLE eq LightingPresets" /IM explorer.exe')
 
         request.addfinalizer(teardown)
 
     @pytest.mark.parametrize("exe_file_name", ["MaterialEditor"])
-    def test_OpeningMaterialAssetBrowser(
+    def test_OpeningMaterial_InAssetBrowser_OpenedSuccessfully(
         self, request, launcher_platform, generic_launcher, exe_file_name
     ):
+        """
+        Please review the hydra script run by this test for more specific test info.
+        Tests for Opening File Source Location.
+        """
 
         expected_lines = [
-            "Asset Browser opened: True",
             "beach_parking_1k_iblglobalcm.exr opened with relative path: \dev\Gems\Atom\TestData\TestData\LightingPresets in WindowsExplorer",
         ]
 
         unexpected_lines = [
-            "Trace::Assert",
-            "Trace::Error",
+            # "Trace::Assert",
+            # "Trace::Error",
             "Traceback (most recent call last):",
         ]
 
@@ -59,4 +58,3 @@ class TestOpeningMaterialAssetBrowser(TestAutomationBase):
             halt_on_unexpected=True,
             log_file_name="MaterialEditor.log",
         )
-
--- Gem/PythonTests/Automated/test_suites/periodic/OpeningFileSourceLocation_test_case.py
+++ Gem/PythonTests/Automated/test_suites/periodic/OpeningFileSourceLocation_test_case.py
@@ -7,7 +7,6 @@ or, if provided, by the license below or the license accompanying this file. Do 
 remove or modify any license notices. This file is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 
-
 Opening File Source Location
 
 import azlmbr.materialeditor will fail with a ModuleNotFound error when using this script with Editor.exe
@@ -15,22 +14,18 @@ This is because azlmbr.materialeditor only binds to MaterialEditor.exe and not E
 You need to launch this script with MaterialEditor.exe in order for azlmbr.materialeditor to appear.
 """
 
-
 import os
 import sys
 import ctypes
-import time
-
 import azlmbr.paths
 from editor_python_test_tools import pyside_utils
 from PySide2 import QtWidgets
-import azlmbr.materialeditor.general as general
 
 sys.path.append(os.path.join(azlmbr.paths.devroot, "AtomTest", "Gem", "PythonTests"))
 
-import Automated.atom_utils.material_editor_utils as material_editor
 from Automated.atom_utils.material_editor_utils import MaterialEditorHelper
 
+MATERIAL_NAME = "beach_parking_1k_iblglobalcm.exr"
 
 class TestOpeningFileSourceLocation(MaterialEditorHelper):
     def __init__(self):
@@ -42,7 +37,7 @@ class TestOpeningFileSourceLocation(MaterialEditorHelper):
         Opening File Source Location
 
         Test Steps:
-        1) Open Material Browser
+        1) Initialize QT objects
         2) Navigate to Gems\Atom\TestData\TestData\LightingPresets\beach_parking_1k_iblglobalcm.exr in Material Browser
         3) Right click on beach_parking_1k_iblglobalcm.exr and Select 'Open in Explorer'
         4) Make sure beach_parking_1k_iblglobalcm.exr opened with relative path: \dev\Gems\Atom\TestData\TestData\LightingPresets in WindowsExplorer
@@ -54,61 +49,91 @@ class TestOpeningFileSourceLocation(MaterialEditorHelper):
         :return: None
         """
 
-        def titleExists(title):
-            EnumWindows = ctypes.windll.user32.EnumWindows
-            EnumWindowsProc = ctypes.WINFUNCTYPE(
-                ctypes.c_bool, ctypes.POINTER(ctypes.c_int), ctypes.POINTER(ctypes.c_int)
-            )
-            GetWindowText = ctypes.windll.user32.GetWindowTextW
-            GetWindowTextLength = ctypes.windll.user32.GetWindowTextLengthW
-            IsWindowVisible = ctypes.windll.user32.IsWindowVisible
-            status = []
-
-            def foreach_window(hwnd, lParam):
-                if IsWindowVisible(hwnd):
-                    length = GetWindowTextLength(hwnd)
-                    buff = ctypes.create_unicode_buffer(length + 1)
-                    GetWindowText(hwnd, buff, length + 1)
-                    if buff.value == title:
-                        status.append(True)
-                        return False
-                    return True
-
-            EnumWindows(EnumWindowsProc(foreach_window), 0)
-            return len(status) > 0
-
-
-        # 1) Open Material Browser
-        pane_name = "Asset Browser"
-        result = material_editor.is_pane_visible(pane_name)
-        if not result:
-            material_editor.set_pane_visibility(pane_name, True)
-        result = material_editor.is_pane_visible(pane_name)
-        print(f"{pane_name} opened: {result}")
-        general.idle_wait_frames(1)
+        # 1) Initialize QT objects
+        editor_window = pyside_utils.get_editor_main_window()
+        asset_browser = self.get_asset_browser(editor_window)
+        search_frame = asset_browser.findChild(QtWidgets.QFrame, "m_searchWidget")
+        search_bar = search_frame.findChild(QtWidgets.QWidget, "textSearch")
+        tree = asset_browser.findChild(QtWidgets.QTreeView, "m_assetBrowserTreeViewWidget")
 
         # 2) Navigate to Gems\Atom\TestData\TestData\LightingPresets\beach_parking_1k_iblglobalcm.exr in Material Browser
-        editor_window = pyside_utils.get_editor_main_window()
-        asset_browser = editor_window.findChildren(QtWidgets.QWidget, "Asset Browser")[0]
-        search_bar = asset_browser.findChildren(QtWidgets.QLineEdit, "textSearch")[0]
-        search_bar.setText("beach_parking_1k_iblglobalcm.exr")
-        general.idle_wait_frames(1)
+        search_bar.setText(MATERIAL_NAME)
+        self.wait_for_condition(lambda: self.find_material_in_browser(tree) is not None, 2.0)
+        model_index = pyside_utils.find_child_by_pattern(tree, MATERIAL_NAME)
 
         # 3) Right click on beach_parking_1k_iblglobalcm.exr and Select 'Open in Explorer'
-        asset_browser_tree = asset_browser.findChild(QtWidgets.QTreeView, "m_assetBrowserTreeViewWidget")
-        asset_browser_tree.expandAll()
-        model_index = pyside_utils.find_child_by_pattern(asset_browser_tree, "beach_parking_1k_iblglobalcm.exr")
-        pyside_utils.trigger_context_menu_entry(asset_browser_tree, "Open in Explorer", index=model_index)
-        general.idle_wait_frames(1)
+        pyside_utils.item_view_index_mouse_click(tree, model_index)
+        pyside_utils.trigger_context_menu_entry(tree, "Open in Explorer", index=model_index)
 
         # 4) Make sure beach_parking_1k_iblglobalcm.exr opened with relative path: \dev\Gems\Atom\TestData\TestData\LightingPresets in WindowsExplorer
-        while True:
-            if titleExists("LightingPresets"):
-                print("beach_parking_1k_iblglobalcm.exr opened with relative path: \dev\Gems\Atom\TestData\TestData\LightingPresets in WindowsExplorer")
-                exit(0)
-            time.sleep(1)
+        self.wait_for_condition(lambda: self.window_exists("LightingPresets"), 2.0)
+        if self.window_exists("LightingPresets"):
+            print(
+                "beach_parking_1k_iblglobalcm.exr opened with relative path: \dev\Gems\Atom\TestData\TestData\LightingPresets in WindowsExplorer"
+            )
+
+    def get_asset_browser(self, editor_window):
+        """
+        Opens the Asset Browser if not opened already and returns the Qt object of Asset Browser
+        :param editor_window - editor_window Qt object
+        :returns asset_browser - Qt object
+        """
+        asset_browser = self.get_asset_browser_dock_widget(editor_window)
+        if asset_browser is None or not asset_browser.isVisible():
+            action = pyside_utils.find_child_by_pattern(editor_window, {"iconText": "Asset Browser"})
+            action.trigger()
+        self.wait_for_condition(lambda: self.get_asset_browser_dock_widget(editor_window) is not None)
+        asset_browser = self.get_asset_browser_dock_widget(editor_window)
+        return asset_browser
+
+    def get_asset_browser_dock_widget(self, editor_window):
+        """
+        Returns the Qt object of Asset Browser
+        :param editor_window - editor_window Qt object
+        :returns asset_browser - Qt object (QDockWidget)
+        """
+        return editor_window.findChild(QtWidgets.QDockWidget, "Asset Browser_DockWidget")
+
+    def find_material_in_browser(self, tree):
+        """
+        Finds a material in Asset Browser tree
+        :param tree - QTreeView object to be searched
+        :returns QModelIndex if material is found, else None
+        """
+        tree.expandAll()
+        return pyside_utils.find_child_by_pattern(tree, MATERIAL_NAME) is not None
+
+    def window_exists(self, title):
+        """
+        Finds a material is opened or not in Windows Explorer
+        :param title - Window title to be searched
+        :returns 1 If Window found, else 0
+        """
+        EnumWindows = ctypes.windll.user32.EnumWindows
+        EnumWindowsProc = ctypes.WINFUNCTYPE(ctypes.c_bool, ctypes.POINTER(ctypes.c_int), ctypes.POINTER(ctypes.c_int))
+        GetWindowText = ctypes.windll.user32.GetWindowTextW
+        GetWindowTextLength = ctypes.windll.user32.GetWindowTextLengthW
+        IsWindowVisible = ctypes.windll.user32.IsWindowVisible
+        status = []
+
+        def foreach_window(hwnd, lParam):
+            """
+            Finds a window is opened or not based on windows title
+            :param hwnd - Handle to A Window
+            :param lParam - Signed 64-bit value in 64-bit Windows
+            """
+            if IsWindowVisible(hwnd):
+                length = GetWindowTextLength(hwnd)
+                buff = ctypes.create_unicode_buffer(length + 1)
+                GetWindowText(hwnd, buff, length + 1)
+                if buff.value == title:
+                    status.append(True)
+                    return False
+                return True
+
+        EnumWindows(EnumWindowsProc(foreach_window), 0)
+        return len(status) > 0
 
 
 test = TestOpeningFileSourceLocation()
-test.run()
-
+test.run_test()
